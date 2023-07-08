1fe1c1724c89f8dcbff01c3f11fd21377a808743
Adriano Schneider Maximiano <adriano-sm@hotmail.com>
2023-07-08 21:13:30 +0100

    creating the counter and fixing the exporting from home.jsx to index.js

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,57 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import rigoImage from "../../../src/img/rigo-baby.jpg";
+import "../../styles/index.css";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+function SimpleCounter(props) {
+  return (
+    <div>
+      <div className="rigo">
+        <img src={rigoImage} alt="Rigo Baby" />
+      </div>
+      <div className="BigCounter">
+        <div>
+          <i className="far fa-clock"></i>
+        </div>
+        <div className="digFour">{props.digitFour}</div>
+        <div className="digThree">{props.digitThree}</div>
+        <div className="digTwo">{props.digitTwo}</div>
+        <div className="digOne">{props.digitOne}</div>
+      </div>
+    </div>
+  );
+}
+
+SimpleCounter.propTypes = {
+  digitFour: PropTypes.number,
+  digitThree: PropTypes.number,
+  digitTwo: PropTypes.number,
+  digitOne: PropTypes.number,
 };
+
+function Home() {
+  let counter = 0;
+  setInterval(function () {
+    const four = Math.floor(counter / 1000);
+    const three = Math.floor(counter / 100) % 10;
+    const two = Math.floor(counter / 10) % 10;
+    const one = Math.floor(counter / 1) % 10;
+    console.log(one, two, three, four);
+    ReactDOM.render(
+      <SimpleCounter
+        digitOne={one}
+        digitTwo={two}
+        digitThree={three}
+        digitFour={four}
+      />,
+      document.querySelector("#app")
+    );
+    counter++;
+  }, 1000);
+
+  return null;
+}
 
 export default Home;
